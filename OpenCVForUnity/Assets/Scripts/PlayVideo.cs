180277eada569c6a4ff8e6ffbb568956ff2fc129
nickcottrell <nick.a.cottrell@gmail.com>
2017-12-04 14:21:54 -0800

    created a surpise birthday sample app

---
+++ OpenCVForUnity/Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayVideo : MonoBehaviour {
+
+	public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+		movie.loop = true;
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		movie.Play ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
